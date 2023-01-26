873f1344f00e6573c152228663aa9a190d98db72
Annie Tran <88398285+abt14@users.noreply.github.com>
2023-01-25 21:09:07 -0800

    implement header, footer, background

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { useState, useEffect } from "react";
-import styled from 'styled-components';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import styled from "styled-components";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import gradientBackground from "./images/Desktop.svg";
+const Container = styled.div`
+  background-image: url(${gradientBackground});
+  background-size: cover;
+`;
 
 function App() {
-  const [ data, setData ] = useState(null);
-  
-  useEffect(() => {
-		fetch("https://kerckhoff.dailybruin.com/api/packages/flatpages/sundance-2023")
-		.then(res => res.json())
-		.then(res => setData(res.data['article.aml']))
-  }, [])
+  const [data, setData] = useState(null);
 
-  data && console.log(data)
+  useEffect(() => {
+    fetch(
+      "https://kerckhoff.dailybruin.com/api/packages/flatpages/sundance-2023"
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res.data["article.aml"]));
+  }, []);
+
+  data && console.log(data);
 
   return (
-    <div className="App">
-      <Header/>
-      Hello Daily Bruin!
-      <Footer/>
-    </div>
+    <Container>
+      <div className="App">
+        <Header />
+        Hello Daily Bruin!
+        <Footer />
+      </div>
+    </Container>
   );
 }
 
