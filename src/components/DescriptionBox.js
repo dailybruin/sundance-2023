984164708a24321a7363176d4a5171de67bb425d
Tracy Zhao <tracyzhao24@g.ucla.edu>
2023-01-26 19:46:29 -0800

    edits

--- src/components/DescriptionBox.js
+++ src/components/DescriptionBox.js
@@ -1,28 +1,42 @@
 import React from "react";
 import styled from 'styled-components';
-import Box from '../images/DescriptionBox.svg'
+import Box from '../images/DescriptionBox.svg';
+import { mediaQueries } from '../shared/config';
 
 const Container = styled.div`
-position:relative;
-width:80%;
+    position: relative;
+    width: 100vw;
+    margin: auto;
 `
 
 const TextBox = styled.div`
-position:absolute;
-top: 0em;
-width:50%;
-margin:auto;
+    position: absolute;
+    top: 2em;
+    width: 40%;
 
-font-family: 'Sofia Sans', sans-serif;
-font-size: 32px;
+    margin-left: auto;
+    margin-right: auto;
+    left: 0;
+    right: 0;
+    text-align: center;
 
-display:flex;
-color:red;
-text-align:center;
+    font-family: 'Sofia Sans', sans-serif;
+    font-size: 30px;
+    color: black;
+    text-align: center;
+
+    ${mediaQueries.mobile} {
+        width: 60vw;
+        font-size: 20px;
+    }
 `
 
 const Image = styled.div`
-position:relative;
+    margin: auto;
+
+    ${mediaQueries.mobile} {
+        width: 90vw;
+    }
 `
 
 export default function DescriptionBox(props) {
