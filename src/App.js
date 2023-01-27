4213f62dd89205f8803b4c79b0fd5a83bf2ec077
Yifan Tang <yifant@g.ucla.edu>
2023-01-26 20:04:48 -0800

    added Landing image and landing gif component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
 import React, { useState, useEffect } from "react";
-import styled from 'styled-components';
-import './App.css';
-import Header from './components/Header';
-import Footer from './components/Footer';
+import styled from "styled-components";
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import LandingImage from "./components/LandingImage";
 
 function App() {
-  const [ data, setData ] = useState(null);
-  
-  useEffect(() => {
-		fetch("https://kerckhoff.dailybruin.com/api/packages/flatpages/sundance-2023")
-		.then(res => res.json())
-		.then(res => setData(res.data['article.aml']))
-  }, [])
+  const [data, setData] = useState(null);
 
-  data && console.log(data)
+  useEffect(() => {
+    fetch(
+      "https://kerckhoff.dailybruin.com/api/packages/flatpages/sundance-2023"
+    )
+      .then((res) => res.json())
+      .then((res) => setData(res.data["article.aml"]));
+  }, []);
+
+  data && console.log(data);
 
   return (
-    <div className="App">
-      <Header/>
-      Hello Daily Bruin!
-      <Footer/>
-    </div>
+    data && (
+      <div className="App">
+        <Header />
+        <LandingImage data={data} />
+        <Footer />
+      </div>
+    )
   );
 }
 
