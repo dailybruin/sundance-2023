c864d6230a35521ef83db71909f3d31d827e3077
Elaine Lin <elaine0.tw@gmail.com>
2023-01-29 21:44:15 -0800

    Made description box image responsive

--- src/components/DescriptionBox.js
+++ src/components/DescriptionBox.js
@@ -3,17 +3,31 @@ import styled from 'styled-components';
 import Box from '../images/DescriptionBox.svg';
 import { mediaQueries } from '../shared/config';
 
-const Container = styled.div`
+const Container1 = styled.div`
     position: relative;
     width: 100vw;
     margin: auto;
+    height: auto;
 `
+
+const Container = styled.div`
+  width: 100%;
+  height: fit-content;
+  position: relative;
+  background-color: #EDE8E5;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
 
 const TextBox = styled.div`
     position: absolute;
     top: 2em;
-    width: 40%;
+    width: 70%;
+    max-width: 50%;
 
+    margin-top: 3em;
     margin-left: auto;
     margin-right: auto;
     left: 0;
@@ -27,22 +41,20 @@ const TextBox = styled.div`
 
     ${mediaQueries.mobile} {
         width: 60vw;
-        font-size: 20px;
+        font-size: 18px;
     }
 `
 
-const Image = styled.div`
-    margin: auto;
-
-    ${mediaQueries.mobile} {
-        width: 90vw;
-    }
+const Image = styled.img`
+    width: 80%;
+    margin-top: 5em;
+    margin-bottom: 5em;
 `
 
 export default function DescriptionBox(props) {
     return (
         <Container>
-            <Image><img src={Box}/></Image>
+            <Image src={Box}/>
             <TextBox>{props.description_box}</TextBox>
         </Container>
     )
